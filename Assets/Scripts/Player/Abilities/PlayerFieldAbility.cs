43c5f8ff349b609f15df63b6961498343fd9a8bc
Steven Henry <stevennassef97@gmail.com>
2022-07-17 17:42:05 +0200

    Ability Update

--- Assets/Scripts/Player/Abilities/PlayerFieldAbility.cs
+++ Assets/Scripts/Player/Abilities/PlayerFieldAbility.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player.Abilities
 {
     public class PlayerFieldAbility : PlayerAbility
     {
+        [SerializeField] private Transform fieldTransform;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            fieldTransform.gameObject.SetActive(false);
+        }
+
         protected override void Start()
         {
             base.Start();
             AbilityAction = InputController.MainInput.Player.Field;
             AbilityAction.performed += OnAbilityPerformed;
+            AbilityPerformed += SetAbilityField;
+        }
+
+        private void SetAbilityField(bool enable)
+        {
+            fieldTransform.gameObject.SetActive(enable);
         }
 
         private void OnDestroy()
         {
             AbilityAction.performed -= OnAbilityPerformed;
+            AbilityPerformed -= SetAbilityField;
         }
     }
 }
