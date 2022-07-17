2c3349af5908fc882a57efcade66cba2d7cb3311
Steven Henry <stevennassef97@gmail.com>
2022-07-17 07:10:38 +0200

    Switching Base Logic

---
+++ Assets/Scripts/Core/PlayerData.cs
@@ -0,0 +1,20 @@
+
+using Essentials;
+using UnityEngine;
+
+namespace Core
+{
+    public class PlayerConfig : ScriptableObject
+    {
+        
+    }
+
+    public class PlayerProperties
+    {
+        private float movementSpeed;
+        private int damage;
+        // private Vector2
+    }
+    
+    // public class PlayerDataDictionary : UnitySerializedDictionary<ePlayerType, PlayerDa>
+}
---
+++ Assets/Scripts/Core/ePlayerType.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Core
+{
+    public enum ePlayerType
+    {
+        Toktok,
+        Bus,
+        MotorCycle
+    }
+}
